fe8fbf9c11bfb0252044dd40ea7b34aad624911f
arjiomega <richardjoseph.omega@gmail.com>
2023-12-09 15:32:53 +0000

    minor fix

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,7 +5,9 @@ from src.data.data_utils import load_list, load_label_dict, remove_unlabeled, pr
 
 if __name__ == '__main__':
 
-    img_list, mask_list = load_list.load(config.RAW_IMG_DIR,config.RAW_MASK_DIR)
+    data_list = load_list.load(config.RAW_IMG_DIR,config.RAW_MASK_DIR)
+    img_list = [f"{img}.jpg" for img in data_list]
+    mask_list = [f"{mask}.png" for mask in data_list]
     specie_dict = load_label_dict.load(config.RAW_DATA_DIR,Path("annotations","list.txt"))
     img_list, mask_list = remove_unlabeled.remove(img_list,mask_list,specie_dict) # type: ignore
 
