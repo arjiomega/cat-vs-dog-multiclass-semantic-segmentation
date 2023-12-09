328d5993f8ce820f89e24745c929436d70ea5e32
arjiomega <richardjoseph.omega@gmail.com>
2023-12-09 13:11:54 +0800

    add inputs

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
+from config import config
 from src.data.data_utils import load_list, load_label_dict, remove_unlabeled, preprocess
 
 if __name__ == '__main__':
 
-    img_list, mask_list = load_list.load()
-    specie_dict = load_label_dict.load()
+    img_list, mask_list = load_list.load(config.RAW_IMG_DIR,config.RAW_MASK_DIR)
+    specie_dict = load_label_dict.load(config.RAW_DATA_DIR,Path("annotations","list.txt"))
     img_list, mask_list = remove_unlabeled.remove(img_list,mask_list,specie_dict) # type: ignore
 
     # saving
