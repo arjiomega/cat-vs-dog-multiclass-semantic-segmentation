379aafa9e8068f33a2e811d1311a98579174865f
arjiomega <richardjoseph.omega@gmail.com>
2023-12-09 13:05:44 +0800

    fix imports

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,9 +1,9 @@
-from src.data.data_utils import load_data, load_specie_dict, remove_unlabeled, preprocess
+from src.data.data_utils import load_list, load_label_dict, remove_unlabeled, preprocess
 
 if __name__ == '__main__':
 
-    img_list, mask_list = load_data.load()
-    specie_dict = load_specie_dict.load()
+    img_list, mask_list = load_list.load()
+    specie_dict = load_label_dict.load()
     img_list, mask_list = remove_unlabeled.remove(img_list,mask_list,specie_dict) # type: ignore
 
     # saving
