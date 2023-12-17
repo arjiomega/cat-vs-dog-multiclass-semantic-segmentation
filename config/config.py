ecccf16ad7e4a800dc822279fa3e63e61552ab97
arjiomega <richardjoseph.omega@gmail.com>
2023-12-17 22:12:05 +0800

    removed old preprocessed data path

--- config/config.py
+++ config/config.py
@@ -8,14 +8,9 @@ DATA_DIR = Path(BASE_DIR,"data")
 RAW_DATA_DIR = Path(DATA_DIR,"raw")
 RAW_IMG_DIR = Path(RAW_DATA_DIR,"images")
 RAW_MASK_DIR = Path(RAW_DATA_DIR,"annotations","trimaps")
+NEW_DATA_DIR = Path(RAW_DATA_DIR,"new_data")
 
 PROCESSED_DATA_DIR = Path(DATA_DIR,"processed")
-PROCESSED_IMG_DIR = Path(PROCESSED_DATA_DIR,"img")
-PROCESSED_MASK_DIR = Path(PROCESSED_DATA_DIR,"mask")
-
-# Generate Directories
-PROCESSED_IMG_DIR.mkdir(parents=True,exist_ok=True)
-PROCESSED_MASK_DIR.mkdir(parents=True,exist_ok=True)
 
 # Config
 CONFIG_DIR = Path(BASE_DIR,"config")
