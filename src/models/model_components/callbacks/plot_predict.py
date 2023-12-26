44dd402922ca1c5b777bc24f33f39b70b5df5b61
arjiomega <richardjoseph.omega@gmail.com>
2023-12-26 19:07:02 +0800

    fix for pep8

--- src/models/model_components/callbacks/plot_predict.py
+++ src/models/model_components/callbacks/plot_predict.py
@@ -7,61 +7,61 @@ import matplotlib.pyplot as plt
 
 from config import config
 
+
 class plot_predict_per_epoch(tf.keras.callbacks.Callback):
-    def __init__(self,model,sample_img,sample_mask,threshold=0.5):
-        super(plot_predict_per_epoch,self).__init__()
+    def __init__(self, model, sample_img, sample_mask, threshold=0.5):
+        super(plot_predict_per_epoch, self).__init__()
         self.model = model
         self.threshold = threshold
         self.sample_img = self.remove_normalization(sample_img)
         self.sample_mask = sample_mask
         self.predict_input = np.expand_dims(sample_img, axis=0)
 
-    def remove_normalization(self,img_input):
+    def remove_normalization(self, img_input):
         # remove -1,1 img normalization
-        return ((img_input+1.0) * 127.5)/255.0
-    
+        return ((img_input + 1.0) * 127.5) / 255.0
+
     def postprocess_predict(self):
         predict_mask = self.model.predict(self.predict_input)
-        predict_mask = np.where(predict_mask>self.threshold,1,0)
+        predict_mask = np.where(predict_mask > self.threshold, 1, 0)
         return predict_mask
 
     def gen_plot(self):
         predict_mask = self.postprocess_predict()
 
-        labels = ['img','bg','cat','dog']
+        labels = ["img", "bg", "cat", "dog"]
 
         true_plot_list = [
-            self.sample_img,                    # Image
-            self.sample_mask[...,0].squeeze(),  # Background
-            self.sample_mask[...,1].squeeze(),  # Cat
-            self.sample_mask[...,2].squeeze()   # Dog
+            self.sample_img,  # Image
+            self.sample_mask[..., 0].squeeze(),  # Background
+            self.sample_mask[..., 1].squeeze(),  # Cat
+            self.sample_mask[..., 2].squeeze(),  # Dog
         ]
 
         predict_plot_list = [
-            self.sample_img,                # Image
-            predict_mask[...,0].squeeze(),  # Background
-            predict_mask[...,1].squeeze(),  # Cat
-            predict_mask[...,2].squeeze()   # Dog
+            self.sample_img,  # Image
+            predict_mask[..., 0].squeeze(),  # Background
+            predict_mask[..., 1].squeeze(),  # Cat
+            predict_mask[..., 2].squeeze(),  # Dog
         ]
 
-        fig,arr = plt.subplots(2,4)
+        fig, arr = plt.subplots(2, 4)
 
-        for i,arr_i in enumerate(arr):
-            for j,arr_j in enumerate(arr_i):
+        for i, arr_i in enumerate(arr):
+            for j, arr_j in enumerate(arr_i):
                 title = "true" if i == 0 else "predict"
                 plot_ = true_plot_list[j] if i == 0 else predict_plot_list[j]
-    
+
                 arr_j.set_title(f"{title}_{labels[j]}")
                 arr_j.imshow(plot_)
-                arr_j.axis('off')
+                arr_j.axis("off")
 
         return fig
 
-    def save_fig(self,fig,epoch):
+    def save_fig(self, fig, epoch):
         file_name = f"predict_plot_epoch_{epoch}.png"
-        fig.savefig(fname=Path(config.FIGURE_DIR,file_name),
-                     format='png')
-        
-    def on_epoch_end(self,epoch,logs=None):
+        fig.savefig(fname=Path(config.FIGURE_DIR, file_name), format="png")
+
+    def on_epoch_end(self, epoch, logs=None):
         fig = self.gen_plot()
-        self.save_fig(fig,epoch)
+        self.save_fig(fig, epoch)
--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -7,17 +7,20 @@ import mlflow
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def preprocess(img):
-    #resize
-    preprocess_img = cv2.resize(img, (224,224), interpolation=cv2.INTER_LINEAR)
-    #normalize
-    preprocess_img = (preprocess_img/127.5) - 1.0
+    # resize
+    preprocess_img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_LINEAR)
+    # normalize
+    preprocess_img = (preprocess_img / 127.5) - 1.0
 
     return preprocess_img
 
+
 def remove_normalization(img):
     # remove -1,1 img normalization
-    return ((img+1.0) * 127.5)/255.0
+    return ((img + 1.0) * 127.5) / 255.0
+
 
 def img_loader(url):
     response = urllib.request.urlopen(url)
@@ -28,7 +31,8 @@ def img_loader(url):
 
     return img
 
-def predict(model,img):
+
+def predict(model, img):
     # preprocess for model prediction
     preprocess_img = np.expand_dims(img, axis=0)
 
@@ -40,41 +44,57 @@ def predict(model,img):
 
     return predict_mask
 
-def gen_plot(img,predict_mask):
+
+def gen_plot(img, predict_mask):
     sample_img = remove_normalization(img)
 
-    labels = ['img','bg','cat','dog','all']
+    labels = ["img", "bg", "cat", "dog", "all"]
 
-    test = np.argmax(predict_mask[0],axis=-1)
+    test = np.argmax(predict_mask[0], axis=-1)
 
     predict_plot_list = [
-        sample_img,                # Image
-        predict_mask[...,0].squeeze(),  # Background
-        predict_mask[...,1].squeeze(),  # Cat
-        predict_mask[...,2].squeeze(),  # Dog
-        test
+        sample_img,  # Image
+        predict_mask[..., 0].squeeze(),  # Background
+        predict_mask[..., 1].squeeze(),  # Cat
+        predict_mask[..., 2].squeeze(),  # Dog
+        test,
     ]
 
-    fig,arr = plt.subplots(1,5)
+    fig, arr = plt.subplots(1, 5)
 
-    for i,arr_i in enumerate(arr):
+    for i, arr_i in enumerate(arr):
         title = "predict"
         plot_ = predict_plot_list[i]
 
         arr_i.set_title(f"{title}_{labels[i]}")
         arr_i.imshow(plot_)
-        arr_i.axis('off')
+        arr_i.axis("off")
 
     return fig
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_uri',dest='model_uri',type=str,help="model_uri can be found in mlflow experiment run artifacts")
-    parser.add_argument('--img_url',dest='img_url',type=str,help="img url that is going to be used to generate a predict mask")
-    parser.add_argument('--save_path',dest='save_path',type=str,help="path where you want to save prediction mask")
+    parser.add_argument(
+        "--model_uri",
+        dest="model_uri",
+        type=str,
+        help="model_uri can be found in mlflow experiment run artifacts",
+    )
+    parser.add_argument(
+        "--img_url",
+        dest="img_url",
+        type=str,
+        help="img url that is going to be used to generate a predict mask",
+    )
+    parser.add_argument(
+        "--save_path",
+        dest="save_path",
+        type=str,
+        help="path where you want to save prediction mask",
+    )
     input_args = parser.parse_args()
-    
+
     if input_args.model_uri and input_args.img_url:
         IMG_URL = input_args.img_url
         model = mlflow.tensorflow.load_model(input_args.model_uri)
@@ -83,13 +103,13 @@ if __name__ == "__main__":
 
     # load img from url
     img = img_loader(IMG_URL)
-    
+
     # preprocess img for prediction
     preprocess_img = preprocess(img)
-    
+
     # predict mask
-    predict_mask = predict(model,preprocess_img)
-    
+    predict_mask = predict(model, preprocess_img)
+
     # generate figure
-    fig = gen_plot(preprocess_img,predict_mask)
+    fig = gen_plot(preprocess_img, predict_mask)
     plt.show()
