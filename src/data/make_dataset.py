8b4b307c9117f73ca02f37e66c1d63ff79b2841c
arjiomega <richardjoseph.omega@gmail.com>
2023-12-29 01:44:50 +0800

    add cli args for data split

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import argparse
 import multiprocessing
 from pathlib import Path
 
@@ -5,6 +8,7 @@ import numpy as np
 import pandas as pd
 
 from config import config
+from src.utils.load_args import load_args
 from src.data.generate import loadbasedata, gencustomdata
 
 
@@ -36,14 +40,46 @@ def multiprocess_save_basedata(
         p.join()
 
 
+def load_input_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--train_args",
+        dest="train_args",
+        type=str,
+        help="json filename in config dir containing the training setup. Ex. 'setup1.json'",
+    )
+    input_args = parser.parse_args()
+
+    if not input_args.train_args:
+        parser.error("training setup file is required.")
+
+    return input_args
+
+
 if __name__ == "__main__":
+    # Load CLI args
+    input_args = load_input_args()
+
+    # Training setup
+    args = load_args(config.CONFIG_DIR, input_args.train_args)
+
+    # Remove processed data dir if exists
+    if os.path.exists(config.PROCESSED_DATA_DIR):
+        shutil.rmtree(config.PROCESSED_DATA_DIR)
+
     # load base data
     base_loader = loadbasedata.LoadBaseData(
         labelpath=config.RAW_DATA_DIR,
         labelfilename=Path("annotations", "list.txt"),
         show_logs=True,
     )
-    train_set, valid_set, test_set = base_loader.load_datasets()
+    train_set, valid_set, test_set = base_loader.load_datasets(
+        datasplit=(
+            args["setup"]["train_split"],
+            args["setup"]["valid_split"],
+            args["setup"]["test_split"],
+        )
+    )
 
     multiprocess_save_basedata(
         base_loader, train_set, valid_set, test_set, savepath=config.PROCESSED_DATA_DIR
